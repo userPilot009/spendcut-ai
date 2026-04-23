d60dfcbc603d382fd93d5204e52db27419656b21
userPilot009 <makemoneyfinances@gmail.com>
2026-04-23 20:23:35 +0100

    changing name middleware to proxy in the file

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   let response = NextResponse.next({
     request: {
       headers: request.headers,
